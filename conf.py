3914115e11e0303894bdae016861112f2c45e213
Panakotta00 <panakotta00@gmail.com>
2019-09-23 08:20:42 +0200

    something

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ extensions = [
 
 templates_path = ['_templates']
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['docs', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
